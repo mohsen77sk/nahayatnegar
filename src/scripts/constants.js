9d5d77cb995b18ce51cc5266547ed88fb25b872c
Fatima Nabizadeh <fatemehn@ershan.ir Verified>
2026-09-19 17:01:44 +0330

    feature: header

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,3 +1,4 @@
 export const MoboTraderAppAddress = 'https://mobotrader.nahayatnegar.com/'
 export const NeoTraderAppAddress = 'https://neotrader.nahayatnegar.com/'
 export const ProTraderAppAddress = 'https://neotrader.nahayatnegar.com/'
+export const registerAddress = 'https://reg.nahayatnegar.com/'
